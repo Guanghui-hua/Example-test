8ce46fd3fb6df28bc97022b2a2a389a8faeab5a7
Guanghui-hua <15890947916@163.com>
2021-05-26 16:50:48 +0800

    5月26日作业

--- 练习.R
+++ 练习.R
@@ -1,3 +1,4 @@
+#Homework5
 x <- c(1,1,2,2,3,3,3)
 w <- c("张三","李四","王五","赵六","张一","李七","王九")
 y <- c("女","男","男","女","女","女","男")
@@ -63,3 +64,73 @@ yy <- usr[4]*0.6 #图例左上角的y坐标
 legend('bottomleft',legend=c('Math','English','Chinese'),
        lty=c(1,2,4),pch=c(1,4,2),col=c('red','green','blue'),cex=0.3)
 par(opar)
+
+
+# 写一个函数计算调和级数的和
+sumSer <- function(n)
+{
+   vec<-1:n
+   sum(1/vec)
+}
+
+#主程序
+sumSer(2)
+sumSer(10)
+sumSer(1000)
+
+
+# chv="5-1 is labor day. The day is coming!"
+# 1）这个字符串中一共有多少个字符？
+# 2）"-"出现在字符串中哪个位置？
+# 3）将字符中的i改为I；
+# 4）将此字符串在.处分隔成两句话。
+chv="5-1 is labor day. The day is coming!"
+length(chv)
+
+
+
+
+# 1.	写一个函数tolplot，既可以画pichart,又可画barplot。以下列数据为例
+library(vcd)
+counts <- table(Arthritis$Improved)
+
+tolplot <- function(type="bar")
+{
+   switch(type,
+          "bar"=barplot(counts),
+         "pi"=pie(counts))
+}
+
+tolplot("bar")
+tolplot("pi")
+
+# 2. 写一个函数myplotter,可以画任意的三角函数，比如正弦 函数等
+
+
+
+
+
+
+# 1）把全国农村承包耕地流转面积（亿亩）与年份做成柱状图
+# 2）把全国农村承包耕地流转面积数据画成直方图，并添加上密度曲线以及正态密度曲线
+
+
+
+
+
+# 1.We want to analyze the stock of International Business Machines Corporation (IBM) based on the its historical prices since April 10, 2017 .
+# 1).  Please write a function which can compute the minimum, maximum, median and sd of a vector. And compute the four values of the close price, open price, low price and high price with your function.
+# 2).  Please compute the daily return of the stock  based on the closed price.
+# 3). Please plot the histogram and density of the daily return. Compare it with a normal curve.  
+# 4).  Please plot the boxplot of the four values close price, open price, low price and high price . Also the  violin plot  of them.
+# 5).  Please create a bar plot that represents  means of  the close price, open price, low price and high price.
+# 2.p2p数据中对6个网贷平台进行了多次观测，利用该数据完成以下问题：
+# 1）.是不是所有平台的credit变量取值不随时间变化？
+# 2）若credit变量取值有随时间变化的，找出来是该平台的id.
+
+
+
+
+
+
+
